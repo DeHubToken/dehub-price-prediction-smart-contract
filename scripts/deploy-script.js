814ff6d045edc4a31d6188cb0acca2c6e060ce60
sbvhev <rohannepal200@gmail.com>
2021-11-12 10:46:17 -0500

    feat: add deploy script

--- scripts/deploy-script.js
+++ scripts/deploy-script.js
@@ -1,19 +1,33 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
 async function main() {
+    let accounts = await hre.ethers.getSigners();
+    let admin = accounts[0];
+    let operator = accounts[1];
+    let adminAddress = await admin.getAddress();
+    let operatorAddress = await operator.getAddress();
 
+    const Prediction = await hre.ethers.getContractFactory('BNBPricePrediction');
+    const prediction = await Prediction.deploy(
+        "", //_oracle BNB / USD, 8 DEC, mainnet: 0x0567F2323251f0Aab15c8dFb1967E4e8A7D42aeE
+        adminAddress,                                 //_adminAddress
+        operatorAddress,                              //_operatorAddress
+        "",                                        //_intervalBlocks
+        "",                                         //_bufferBlocks
+        "",                           //_minBetAmount
+        "",                                        //_oracleUpdateAllowance
+        "", //_BUSD, 0xe9e7cea3dedca5984780bafc599bd69add087d56
+    );
+    await prediction.deployed();
+
+    console.log("Prediction deployed to:", prediction.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
 main()
-  .then(() => process.exit(0))
-  .catch(error => {
-    console.error(error);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(error);
+        process.exit(1);
+    });
